8fdbb77060289f934cee05a83550286acb3b266a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-30 20:10:42 +0200

    Declare a new version AttoLisp object

--- src/attolisp.h
+++ src/attolisp.h
@@ -1,53 +1,53 @@
 #ifndef __ATTOLISP_H_
 #define __ATTOLISP_H_
-#include<stddef.h>
 
-typedef enum {
-    AL_TAG_CONS,
-    AL_TAG_ATOM,
-    AL_TAG_FUNCTION,
-    AL_TAG_LAMBDA
-} al_tag_t;
+#define ATTOLISP_TYPE_INT       1
+#define ATTOLISP_TYPE_CELL      2
+#define ATTOLISP_TYPE_SYMBOL    3
+#define ATTOLISP_TYPE_PRIMITIVE 4
+#define ATTOLISP_TYPE_FUNCTION  5
+#define ATTOLISP_TYPE_MACRO     6
+#define ATTOLISP_TYPE_ENV       7
+#define ATTOLISP_TYPE_MOVED     8
+#define ATTOLISP_TYPE_TRUE      9
+#define ATTOLISP_TYPE_NIL       10
+#define ATTOLISP_TYPE_DOT       11
+#define ATTOLISP_TYPE_PAREN     12
 
-typedef struct al_object al_object_t;
-struct al_object{
-    al_object_t *car;
-    al_object_t *cdr;
-    al_tag_t tag;
-};
+struct al_object_t;
+typedef al_object_t* (*al_primitive_t)(
+    void *root, struct al_object_t **env, struct al_object_t **args
+);
 
-typedef al_object_t* (*al_function_t)(al_object_t *object);
-
-typedef struct {
-    al_object_t *heap;
-    al_object_t *from;
-    al_object_t *to;
-    al_object_t *allocptr;
-    al_object_t *scanptr;
-    size_t nroots;
-    size_t roottop;
-} al_gc_t;
-
-void al_gc_init(void);
-al_object_t* al_gc_alloc(al_tag_t tag, al_object_t *car, al_object_t *cdr);
-void al_gc_protect(al_object_t **root, ...);
-void al_gc_pop(void);
-void al_gc_copy(al_object_t **root);
-void al_gc_collect(void);
-
-// ---
-
-al_object_t* al_car(al_object_t *object);
-al_object_t* al_cdr(al_object_t *object);
-al_object_t* al_cons(al_object_t *object);
-al_object_t* al_equalp(al_object_t *object);
-al_object_t* al_pairp(al_object_t *object);
-al_object_t* al_nullp(al_object_t *object);
-al_object_t* al_sum(al_object_t *object);
-al_object_t* al_sub(al_object_t *object);
-al_object_t* al_mul(al_object_t *object);
-al_object_t* al_print(al_object_t *object); // display
-al_object_t* al_newline(al_object_t *object);
-al_object_t* al_read(al_object_t *object);
+typedef struct al_object_t {
+    int type;   /* object type */
+    int size;   /* total size  */
+    union{
+        // integer
+        int value;
+        // cell
+        struct{
+            struct al_object_t *car;
+            struct al_object_t *cdr;
+        };
+        // symbol
+        char name[1];
+        // primive function
+        al_primitive_t fn;
+        // macro
+        struct {
+            struct al_object_t *params;
+            struct al_object_t *body;
+            struct al_object_t *env;
+        };
+        // environment frame
+        struct {
+            struct al_object_t *vars;
+            struct al_object_t *up;
+        };
+        // forwarding pointer
+        void *moved;
+    };
+} al_object_t;
 
 #endif
